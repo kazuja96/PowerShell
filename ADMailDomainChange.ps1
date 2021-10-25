9b2a6a1f82215b3153a24921e144aa5a6ac2994d
kazuja96 <67233006+kazuja96@users.noreply.github.com>
2021-10-25 16:03:27 +0200

    Add files via upload

---
+++ ADMailDomainChange.ps1
@@ -0,0 +1,23 @@
+﻿#Skrypt zmieniający domenę w adresach mailowych wszystkich użytkowników w Active Directory
+
+Import-Module ActiveDirectory
+
+#Importuję moduł Active directory.
+#Jest niezbędny do uruchomienia komend Get-ADUser oraz Set-ADUser.#>
+
+$ADusers = Get-ADUser -Filter *
+#Tworzę kolekcję ADusers. Zawiera ona wszystkich użytkowników w AD.
+
+
+foreach ($ADuser in $ADusers) #Rozpoczynam pętlę foreach dla kolekcji ADusers. Każdy pojedyńczy obiekt oznaczamy jako ADuser.
+{
+    $email = $ADuser.samaccountname + '@domena.pl'
+    #Tworzę zmienną email. 
+    #Zmienna zawiera nazwę użytkownika wraz z dodanym przyrostkiem @domena.pl. 
+    #np. dla użytkownika jan.kowalski będzie to jan.kowalski@domena.pl
+
+    Set-ADUser -Identity $ADuser.samaccountname -EmailAddress $email
+    #Ustawiam wyżej utworzoną zmienną jako adres mailowy.
+
+}
+#Zawartość pętli wykona się dla każdego użytkownika w kolekcji $ADusers.
