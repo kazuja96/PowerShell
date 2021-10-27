e950450036783495a8187b489ed732cf18e37744
kazuja96 <67233006+kazuja96@users.noreply.github.com>
2021-10-27 15:06:18 +0200

    Update ADMailDomainChangeEx2.ps1

--- ADMailDomainChangeEx2.ps1
+++ ADMailDomainChangeEx2.ps1
@@ -9,13 +9,13 @@ $ADusers = Get-ADUser -Filter *
 #Tworzenie kolekcji $ADusers. Kolekcja zawiera wszystkich użytkowników w AD.
 
 #Przesyłanie zawartości $ADusers do ForEach-Object za pomocą pipeline.
-#W tym wypadku do identyfikacji pojedyńczego obiektu w kolekcji użyta jest zmienna $_ zamiast $ADuser
+#W tym wypadku do identyfikacji pojedyńczego obiektu w kolekcji użyta jest zmienna $_ zamiast $ADuser.
 $ADusers | ForEach-Object{
 
     $email = $_.samaccountname + '@domena.pl'
     #Tworzenie zmiennej $email. 
     #Zmienna zawiera nazwę użytkownika wraz z dodanym przyrostkiem @domena.pl. 
-    #np. dla użytkownika jan.kowalski będzie to jan.kowalski@domena.pl
+    #np. dla użytkownika jan.kowalski będzie to jan.kowalski@domena.pl.
 
     Set-ADUser -Identity $_.samaccountname -EmailAddress $email
     #Ustawienie wyżej utworzonej zmiennej jako adres mailowy.
