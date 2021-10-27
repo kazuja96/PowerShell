8102aa1ee6c5206580b63fb82dc06f886a4cb520
kazuja96 <67233006+kazuja96@users.noreply.github.com>
2021-10-27 14:41:51 +0200

    Update ImportCsvExample.ps1

--- ImportCsvExample.ps1
+++ ImportCsvExample.ps1
@@ -1,9 +1,13 @@
+#Skrypt importujący numery telefonów z pliku csv.
+
+#Importuję moduł Active directory.
+#Jest niezbędny do uruchomienia komendy Set-ADUser.
 Import-Module ActiveDirectory
 
 #Importuję plik numbers.csv, znajdujący się w lokalizacji C:\temp\numbers.csv do $NumbersList.
 $NumbersList = Import-Csv -Path C:\temp\numbers.csv
 
-#Rozpoczynam pętle forach.
+#Rozpoczynam pętle foreach.
 #Pętla wykonuje się dla każdego wiersza w pliku numbers.csv.
 foreach($Number in $NumbersList){
 
