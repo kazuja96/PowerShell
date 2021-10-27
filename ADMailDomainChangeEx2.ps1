74306038532607d5ae9f993a82224e14e55b1ed6
kazuja96 <67233006+kazuja96@users.noreply.github.com>
2021-10-27 14:52:29 +0200

    Update ADMailDomainChangeEx2.ps1

--- ADMailDomainChangeEx2.ps1
+++ ADMailDomainChangeEx2.ps1
@@ -2,23 +2,23 @@
 #W tym przykładzie użyję ForEach-Object.
 
 Import-Module ActiveDirectory
-#Importuję moduł Active directory.
+#Import modułu Active directory.
 #Jest niezbędny do uruchomienia komend Get-ADUser oraz Set-ADUser.
 
 $ADusers = Get-ADUser -Filter *
-#Tworzę kolekcję ADusers. Zawiera ona wszystkich użytkowników w AD.
+#Tworzenie kolekcji $ADusers. Kolekcja zawiera wszystkich użytkowników w AD.
 
-#Przesyłamy zawartość ADusers do ForEach-Object za pomocą pipeline.
-#W tym wypadku do identyfikacji pojedyńczego obiektu w kolekcji użyjemy zmiennej $_ zamiast $ADuser
+#Przesyłanie zawartości $ADusers do ForEach-Object za pomocą pipeline.
+#W tym wypadku do identyfikacji pojedyńczego obiektu w kolekcji użyta jest zmienna $_ zamiast $ADuser
 $ADusers | ForEach-Object{
 
     $email = $_.samaccountname + '@domena.pl'
-    #Tworzę zmienną email. 
+    #Tworzenie zmiennej $email. 
     #Zmienna zawiera nazwę użytkownika wraz z dodanym przyrostkiem @domena.pl. 
     #np. dla użytkownika jan.kowalski będzie to jan.kowalski@domena.pl
 
     Set-ADUser -Identity $_.samaccountname -EmailAddress $email
-    #Ustawiam wyżej utworzoną zmienną jako adres mailowy.
+    #Ustawienie wyżej utworzonej zmiennej jako adres mailowy.
     
 }
 #Zawartość pętli wykona się dla każdego użytkownika w kolekcji $ADusers.
